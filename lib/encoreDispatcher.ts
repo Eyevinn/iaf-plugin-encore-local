ce98ae754e9f3084746d1aad9728d7bc13b93380
Oscar Nord <oscar.nord@gmail.com>
2021-10-06 22:12:35 +0200

    minor updates to encoreDispatcher

--- lib/encoreDispatcher.ts
+++ lib/encoreDispatcher.ts
@@ -37,27 +37,33 @@ export class EncoreDispatcher implements TranscodeDispatcher {
   async getJobs(page: number, size: number): Promise<any> {
     this.logger.info('Fetching jobs from Encore');
     const url = `${this.encoreEndpoint}/encoreJobs?page=${page}&size=${size}`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-    const json = await response.json();
-    return json;
+    try {
+      const resp = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      return resp.json();
+    } catch (err) {
+      this.logger.error(err);
+    }
   }
 
   async getJob(jobId: string): Promise<any> {
     this.logger.info('Fetching job from Encore');
     const url = `${this.encoreEndpoint}/encoreJobs/${jobId}`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-    const json = await response.json();
-    return json;
+    try {
+      const resp = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      return resp.json();
+    } catch (err) {
+      this.logger.error(err);
+    }
   }
 
   async createJobs(fileName: string): Promise<any> {
@@ -68,17 +74,23 @@ export class EncoreDispatcher implements TranscodeDispatcher {
     config.inputs = [{
       "type": "AudioVideo",
       "uri": `${this.inputLocation}/${fileName}`,
+      "params": {
+        "ac": "2"
+      }
     }];
     const url = `${this.encoreEndpoint}/encoreJobs`;
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(this.encodeParams)
-    });
-    const json = await response.json();
-    return json;
+    try {
+      const resp = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: this.encodeParams
+      });
+      return resp.json();
+    } catch (err) {
+      this.logger.error(err);
+    }
   }
 
   loadEncodeParams(templateFileName: string) {
