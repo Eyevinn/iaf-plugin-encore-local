86932fbf1bc9e23af83808eb4b90b755c11cdd2c
Oscar Nord <oscar.nord@gmail.com>
2021-10-14 15:12:50 +0200

    chore: minor cleanup in smilGenerator

--- lib/utils/smilGenerator.ts
+++ lib/utils/smilGenerator.ts
@@ -13,23 +13,23 @@ export function getFilesByName(dir: string, fileName: string): string[] {
 export function createSMILFile(dir: string, fileName: string): void {
   const outputFolder = path.join(dir, path.basename(fileName, path.extname(fileName)));
   let files: string[] = getFilesByName(outputFolder, fileName);
-  let smil  = `<?xml version="1.0" encoding="UTF-8"?>`;
-  smil += `<smil>`;
-  smil += `<body>`;
-  smil += `<alias value="${fileName}" />`;
-  smil += `<switch>`;
+  let content  = `<?xml version="1.0" encoding="UTF-8"?>`;
+  content += `<smil>`;
+  content += `<body>`;
+  content += `<alias value="${fileName}" />`;
+  content += `<switch>`;
   for (let i = 0; i < files.length; i++) {
     let file = files[i];
     if (file.includes("SURROUND")) {
-      smil += `<audio name="${file}" systemLanguage="eng" subtitleName="SURROUND"/>`;
+      content += `<audio name="${file}" systemLanguage="eng" subtitleName="SURROUND"/>`;
     } else if (file.includes("STEREO")) {
-      smil += `<audio name="${file}" systemLanguage="eng" subtitleName="STEREO"/>`;
+      content += `<audio name="${file}" systemLanguage="eng" subtitleName="STEREO"/>`;
     } else if (!file.includes("thumb") && file.includes("mp4")) { 
-      smil += `<video name="${file}" systemLanguage="eng" audioName="English"/>`;
+      content += `<video name="${file}" systemLanguage="eng" audioName="English"/>`;
     }
   }
-  smil += `</switch>`;
-  smil += `</body>`;
-  smil += `</smil>`;
-  fs.writeFileSync(path.join(outputFolder, `${fileName}-config.smil`), smil);
+  content += `</switch>`;
+  content += `</body>`;
+  content += `</smil>`;
+  fs.writeFileSync(path.join(outputFolder, `${path.basename(fileName, path.extname(fileName))}.smil`), content);
 }
